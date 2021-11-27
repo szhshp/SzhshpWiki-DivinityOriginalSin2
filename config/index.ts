9adf0c5367b112346f14e2717ee7a690b1dc2390
zill.Office <24031shp@sina.com>
2021-11-27 22:24:39 +0800

    feat: index page template

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,5 @@
 export const SITE = {
-  NAME: "神界原罪合成表 速查 - Szhshp Games",
-  NETEASE_ENDPOINT: "https://integration.neteastmusic.szhshp.org",
+  NAME: "神界原罪合成表攻略速查 - Szhshp Games",
 };
 
 export const DEBUG = process.env.NEXT_PUBLIC_DEBUG === "true";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,41 @@
 import React from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
-import { Grid } from "@mui/material";
+import { Grid, Box } from "@mui/material";
 import { Content } from "components/content";
 import { SITE } from "config";
+import { crafting } from "data";
 
-const Home: NextPage = ({  }) => (
+const columns: GridColDef[] = [
+  { field: "Result 1", headerName: "名称", width: 250 },
+  { field: "Category", headerName: "类别", width: 200 },
+  { field: "Effect 1", headerName: "效果", width: 250 },
+  ...Array(5)
+    .fill(undefined)
+    .map((e, i) => ({
+      field: `Ingredient ${i + 1}`,
+      headerName: `素材 ${i + 1}`,
+      width: 250,
+    })),
+];
+
+const Home: NextPage = () => (
   <Content>
     <Head>
       <title>{SITE.NAME}</title>
     </Head>
     <Grid container item xs={12}>
+      <Box minHeight="100vh" width="100%">
+        <DataGrid
+          rows={crafting.map((e, i) => ({
+            ...e,
+            id: i,
+          }))}
+          columns={columns}
+        />
+      </Box>
     </Grid>
   </Content>
 );
