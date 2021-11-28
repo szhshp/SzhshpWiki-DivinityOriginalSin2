511049433f4189a72c721c449585242b6d094f66
zill.Office <24031shp@sina.com>
2021-11-28 12:49:58 +0800

    refactor: update sidebar to be swiperable

--- components/content/Content.tsx
+++ components/content/Content.tsx
@@ -1,137 +1,30 @@
-import { SITE, ROUTES } from "config";
-import * as React from "react";
-import { styled, useTheme } from "@mui/material/styles";
+import React, { ReactElement, useState } from "react";
 import {
+  AppBar,
   Box,
-  Drawer,
+  Grid,
   Toolbar,
-  List,
-  Typography,
-  Divider,
   IconButton,
-  CssBaseline,
-  ListItemButton,
+  Drawer,
+  List,
   ListItem,
+  ListItemButton,
 } from "@mui/material";
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
-
-import {
-  Menu as MenuIcon,
-  ChevronLeft as ChevronLeftIcon,
-} from "@mui/icons-material";
-
 import { SLink } from "components/general/SLink";
+import { ROUTES } from "config";
+import { Menu as MenuIcon } from "@mui/icons-material";
 
-const drawerWidth = 200;
-
-const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
-  open?: boolean;
-}>(({ theme, open }) => ({
-  flexGrow: 1,
-  padding: theme.spacing(1),
-  transition: theme.transitions.create("margin", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  marginLeft: `-${drawerWidth}px`,
-  ...(open && {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  }),
-}));
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})<AppBarProps>(({ theme, open }) => ({
-  transition: theme.transitions.create(["margin", "width"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-  justifyContent: "flex-end",
-}));
-
-export const Content = ({
-  children,
-}: {
-  children: JSX.Element | JSX.Element[];
-}) => {
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
+const Topbar = (): ReactElement => {
+  const [drawOpen, setDrawOpen] = useState(true);
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar position="fixed" open={open}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{ mr: 2, ...(open && { display: "none" }) }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            {SITE.NAME}
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
-            width: drawerWidth,
-            boxSizing: "border-box",
-          },
-        }}
-        variant="persistent"
-        anchor="left"
-        open={open}
-      >
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
+    <Box sx={{ flexGrow: 1 }}>
+      <Drawer anchor="left" open={drawOpen} onClose={() => setDrawOpen(false)}>
+        <List sx={{ width: "240px" }}>
           {ROUTES.map((item) => (
             <ListItem button key={item.href}>
               <ListItemButton role={undefined} dense>
                 <SLink href={item.href}>
-                  <Box>
+                  <Box px={2} py={1}>
                     {item.label}
                   </Box>
                 </SLink>
@@ -140,10 +33,33 @@ export const Content = ({
           ))}
         </List>
       </Drawer>
-      <Main open={open}>
-        <DrawerHeader />
-        {children}
-      </Main>
+      <AppBar position="sticky">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={() => setDrawOpen(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     </Box>
   );
 };
+
+export const Content = ({
+  children,
+}: {
+  children: JSX.Element | JSX.Element[];
+}): JSX.Element => (
+  <Grid container>
+    <Topbar />
+    <Grid item xs={12}>
+      {children}
+    </Grid>
+  </Grid>
+);
