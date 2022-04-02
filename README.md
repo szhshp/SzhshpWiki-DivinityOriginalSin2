5191812592a6541ac05603ac2b14eaf2c0da5df9
szhielelp <24031shp@sina.com>
2022-04-02 13:47:17 +0800

    chore: readme

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Szhshp Wiki - Divinity: Original Sin 2
 
+
+> 项目基于 [Project Titan](https://github.com/szhshp/NextJS-BlogTemplate-ProjectTitan)
+
 szhshp 的攻略 - 神界原罪 2 合成表攻略速查
 
 官方攻略加载太慢所以我自个儿写了个网站方便搜索
--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,5 @@
 export const SITE = {
-  NAME: "Szhshp Wiki - Divinity: Original Sin 2",
+  NAME: "szhshp 的攻略 - 神界原罪2合成表攻略速查 - Szhshp Wiki - Divinity: Original Sin 2",
 };
 
 export const DEBUG = process.env.NEXT_PUBLIC_DEBUG === "true";
